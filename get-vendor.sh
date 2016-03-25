c785ca0a080bac38dc277cd38dcdfa42d1db16b3
Cai Guanhao (Choi Goon-ho) <caiguanhao@gmail.com>
2016-03-25 11:04:06 +0800

    update get-vendor.sh

--- get-vendor.sh
+++ get-vendor.sh
@@ -2,32 +2,32 @@
 
 set -e
 
+SHADOWSOCKS_GO_COMMIT=1.1.4
+CHACHA20_COMMIT=ec07b4f69a3f70b1dd2a8ad77230deb1ba5d6953
+CRYPTO_COMMIT=c197bcf24cde29d3f73c7b4ac6fd41f4384e8af6
+
+rm -rf vendor
 mkdir -p vendor
 
-if test ! -d vendor/shadowsocks-go-1.1.4; then
-  echo "getting shadowsocks-go-1.1.4..."
-  curl -Ls https://github.com/shadowsocks/shadowsocks-go/archive/1.1.4.tar.gz | tar -C vendor -xzf - shadowsocks-go-1.1.4/shadowsocks
+if test ! -d vendor/shadowsocks-go-$SHADOWSOCKS_GO_COMMIT; then
+  echo "getting shadowsocks-go-$SHADOWSOCKS_GO_COMMIT..."
+  curl -Ls https://github.com/shadowsocks/shadowsocks-go/archive/$SHADOWSOCKS_GO_COMMIT.tar.gz | \
+    tar -C vendor -xvzf - shadowsocks-go-$SHADOWSOCKS_GO_COMMIT/shadowsocks
 fi
 
-if test ! -d vendor/chacha20; then
-  echo "getting chacha20..."
-  curl -Ls https://github.com/codahale/chacha20/archive/master.tar.gz | tar -C vendor -xzf -
-  mv vendor/chacha20-master vendor/chacha20
+if test ! -d vendor/chacha20-$CHACHA20_COMMIT; then
+  echo "getting chacha20-$CHACHA20_COMMIT..."
+  curl -Ls https://github.com/codahale/chacha20/archive/$CHACHA20_COMMIT.tar.gz | tar -C vendor -xvzf -
 fi
 
-grep --include '*.go' -lrF 'github.com/codahale/chacha20' vendor | \
-  xargs sed -i '' 's,github.com/codahale/chacha20,github.com/caiguanhao/shadowsocks-local/vendor/chacha20,'
+grep --include "*.go" -lrF "github.com/codahale/chacha20" vendor | \
+  xargs sed -i "" "s,github.com/codahale/chacha20,github.com/caiguanhao/shadowsocks-local/vendor/chacha20-$CHACHA20_COMMIT,"
 
-if test ! -d vendor/crypto; then
-  echo "getting crypto..."
-  curl -Ls https://github.com/golang/crypto/archive/master.tar.gz | tar -C vendor -xzf -
-  mkdir -p vendor/crypto
-  mv vendor/crypto-master/blowfish vendor/crypto/blowfish
-  mv vendor/crypto-master/cast5 vendor/crypto/cast5
-  mkdir -p vendor/crypto/salsa20
-  mv vendor/crypto-master/salsa20/salsa vendor/crypto/salsa20/salsa
-  rm -rf vendor/crypto-master
+if test ! -d vendor/crypto-$CRYPTO_COMMIT; then
+  echo "getting crypto-$CRYPTO_COMMIT..."
+  curl -Ls https://github.com/golang/crypto/archive/$CRYPTO_COMMIT.tar.gz | \
+    tar -C vendor -xvzf - crypto-$CRYPTO_COMMIT/blowfish crypto-$CRYPTO_COMMIT/cast5 crypto-$CRYPTO_COMMIT/salsa20/salsa
 fi
 
-grep --include '*.go' -lrF 'golang.org/x/crypto' vendor | \
-  xargs sed -i '' 's,golang.org/x/crypto,github.com/caiguanhao/shadowsocks-local/vendor/crypto,'
+grep --include "*.go" -lrF "golang.org/x/crypto" vendor | \
+  xargs sed -i "" "s,golang.org/x/crypto,github.com/caiguanhao/shadowsocks-local/vendor/crypto-$CRYPTO_COMMIT,"
